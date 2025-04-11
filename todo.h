b8dfb3de191d79f1095da09d0ee2c4462c828fdc
junkyo <junkyochang@snu.ac.kr>
2025-04-12 01:54:53 +0900

    tdd-todolist

--- todo.h
+++ todo.h
@@ -1,18 +1,18 @@
 
 class TodoList {
-public:
-    void init();     // replaces constructor
-    void destroy();  // replaces destructor
-
-    void add_task(const char* task);
-    void remove_task(int index);
-    const char** get_pending_tasks(int& count) const;
-
-private:
-    static const int MAX_TASKS = 32;
-    char* tasks[MAX_TASKS];
-    int size;
-
-    int string_length(const char* str) const;
-    void string_copy(char* dest, const char* src) const;
-};
+    public:
+        void init();     // replaces constructor
+        void destroy();  // replaces destructor
+    
+        void add_task(const char* task);
+        void remove_task(int index);
+        const char** get_pending_tasks(int& count) const;
+    
+    private:
+        static const int MAX_TASKS = 32;
+        char* tasks[MAX_TASKS];
+        int size;
+    
+        int string_length(const char* str) const;
+        void string_copy(char* dest, const char* src) const;
+    };
